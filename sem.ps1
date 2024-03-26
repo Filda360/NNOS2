3d0c5621294b7d5485ddab95cab66e2f3287d291
Filda360 <82775576+Filda360@users.noreply.github.com>
2024-03-26 09:29:00 +0100

    Add files via upload

---
+++ sem.ps1
@@ -0,0 +1,20 @@
+﻿# Vytvoření pole nodeModulesArray
+$nodeModulesArray = @()
+
+# Získání složek "node_modules" a přidání jejich cest do pole
+Get-ChildItem -Path "C:\Users\42060" -Filter node_modules -Directory -Recurse | Where-Object { $_.FullName -notlike "*\node_modules\*" } | ForEach-Object {
+    $sizeInBytes = (Get-ChildItem -Path $_.FullName -Recurse | Measure-Object -Property Length -Sum).Sum
+    $sizeInMB = [math]::Round($sizeInBytes / 1MB)
+    $lastModified = (Get-ChildItem -Path $_.FullName -Recurse | Sort-Object LastWriteTime -Descending | Select-Object -First 1).LastWriteTime
+    $nodeModulesArray += [PSCustomObject]@{
+        Path = $_.FullName
+        Size_MB = $sizeInMB
+        LastModified = $lastModified
+    }
+}
+
+# Zobrazení pole v interaktivním okně pro výběr více položek
+$selectedItems = $nodeModulesArray | Out-GridView -Title "Vyberte položky" -OutputMode Multiple
+
+# Vypsání vybraných položek
+$selectedItems
