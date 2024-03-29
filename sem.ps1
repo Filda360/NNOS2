2c0c5bf31b51fcee35624bc465e38248f14161b6
Filda360 <Filda360@seznam.cz>
2024-03-29 23:05:57 +0100

    přidán CLI výběr z GitHubu

--- sem.ps1
+++ sem.ps1
@@ -13,8 +13,16 @@ Get-ChildItem -Path "C:\Users\42060" -Filter node_modules -Directory -Recurse | 
     }
 }
 
-# Zobrazení pole v interaktivním okně pro výběr více položek
-$selectedItems = $nodeModulesArray | Out-GridView -Title "Vyberte položky" -OutputMode Multiple
+# VERZE 1 - graficky okno 
+<# $selectedItems = $nodeModulesArray | Out-GridView -Title "Vyberte položky" -OutputMode Multiple #>
+
+# VERZE 2 - CLI (prevzato z: https://github.com/Sebazzz/PSMenu) 
+Import-Module .\PSMenu\PSMenu.psm1 -Verbose
+Clear-Host
+Write-Host "Vyber node_modules k odstranění:"
+Write-Host " [MEZERNIK] - vybrat"
+Write-Host " [ENTER] - potvrdit výběr"
+$selectedItems = Show-Menu -MenuItems $nodeModulesArray -MultiSelect -MenuItemFormatter { $Args | Select -Exp Path }
 
 # Vypsání vybraných položek
-$selectedItems
+Write-Output $selectedItems
