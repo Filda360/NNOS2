73f058607b82bc73db2216530c7e892046d21920
Pavel Klečanský <klecanskypavel@gmail.com>
2024-04-15 23:39:59 +0200

    Add some test cases to show in presentation

---
+++ Test.ps1
@@ -0,0 +1,16 @@
+cd C:\Programming\project_management_bachelor
+. 'C:\Programming\NNOS2\NodeModuleRemover.ps1'
+
+. 'C:\Programming\NNOS2\NodeModuleRemover.ps1' -h
+
+. 'C:\Programming\NNOS2\NodeModuleRemover.ps1' -d C:\Programming\project_management_bachelor
+
+. 'C:\Programming\NNOS2\NodeModuleRemover.ps1' -d C:\Programming\project_management_bachelor -c Red
+
+. 'C:\Programming\NNOS2\NodeModuleRemover.ps1' -d C:\Programming\project_management_bachelor -x
+
+. 'C:\Programming\NNOS2\NodeModuleRemover.ps1' -d C:\Programming\project_management_bachelor -t src -x
+
+. 'C:\Programming\NNOS2\NodeModuleRemover.ps1' -f
+
+. 'C:\Programming\NNOS2\NodeModuleRemover.ps1' -d C:\Programming\project_management_bachelor -c Red -x -e test
